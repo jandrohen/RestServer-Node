573db61f5a8b8a1453b8da2acade7729c6510ada
Jandronit <84868351+Jandronit@users.noreply.github.com>
2021-12-26 19:41:23 +0100

    (lesson 102 course node) Example Request HTTP (GET, POST, PUT, DELETE) Express library

--- models/server.js
+++ models/server.js
@@ -21,8 +21,29 @@ class Server{
     routes() {
 
         this.app.get('/api', (req, res) => {
-            res.send('Hello World');
+            res.json({
+                message: 'get API'
+            });
         });
+
+        this.app.put('/api', (req, res) => {
+            res.json({
+                message: 'put API'
+            });
+        });
+
+        this.app.post('/api', (req, res) => {
+            res.json({
+                message: 'post API'
+            });
+        });
+
+        this.app.delete('/api', (req, res) => {
+            res.json({
+                message: 'delete API'
+            });
+        });
+
     }
 
     listen() {
