939814d7d301581740a8c111d2f8c0440ad2548a
Jandronit <84868351+Jandronit@users.noreply.github.com>
2022-03-08 20:20:13 +0100

    (lesson 177 course node) implement Search controller for search any Category or Product by name or ID

--- controllers/search.contoller.js
+++ controllers/search.contoller.js
@@ -31,6 +31,46 @@ const searchUsers = async (term = '', res = response) =>{
   })
 }
 
+const searchCategories = async (term = '', res = response) =>{
+  const isMongoId = ObjectId.isValid( term );
+
+  if ( isMongoId ) {
+    const category = await Category.findById(term);
+    return  res.json({
+      results: ( category ) ? [ category ] : []
+    })
+  }
+
+  const regex = new  RegExp(term, 'i')
+
+  const categories = await Category.find({ name: regex, state: true});
+
+  res.json({
+    results: categories
+  })
+}
+
+const searchProducts = async (term = '', res = response) =>{
+  const isMongoId = ObjectId.isValid( term );
+
+  if ( isMongoId ) {
+    const product = await Product.findById(term)
+                            .populate('category', 'name');
+    return  res.json({
+      results: ( product ) ? [ product ] : []
+    })
+  }
+
+  const regex = new  RegExp(term, 'i')
+
+  const products = await Product.find({ name: regex, state: true})
+                          .populate('category', 'name');
+
+  res.json({
+    results: products
+  })
+}
+
 
 const search = async (req , res = response) => {
   const { collection, term } = req.params;
@@ -46,8 +86,10 @@ const search = async (req , res = response) => {
       await searchUsers(term,res);
       break;
     case 'categories':
+      await searchCategories(term,res);
       break;
     case 'products':
+      await searchProducts(term,res);
       break;
     default:
       res.status(500).json({
