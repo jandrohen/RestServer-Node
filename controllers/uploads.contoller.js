20df26d97cb782f133a4296a2f0489573ec0e321
Jandronit <84868351+Jandronit@users.noreply.github.com>
2022-03-09 20:22:08 +0100

    (lesson 195-196 course node) Install library cloudinary, create account and change method of uploading files to our server

--- controllers/uploads.contoller.js
+++ controllers/uploads.contoller.js
@@ -1,6 +1,9 @@
 const path = require('path');
 const fs = require("fs");
 
+const cloudinary = require('cloudinary').v2
+cloudinary.config( process.env.CLOUDINARY_URL );
+
 const { response, request } = require("express");
 const { uploadFiles } = require("../helpers");
 
@@ -112,8 +115,58 @@ const viewImage = async (req = request, res = response) => {
   res.sendFile( pathImage )
 }
 
+const updateImageCloudinary = async (req = request, res = response) => {
+
+  const { id, collection } = req.params;
+
+  let model;
+
+  switch (collection) {
+    case "users":
+      model = await User.findById(id);
+      if (!model) {
+        return res.status(400).json({
+          msg: `No existe un usuario con el id ${id}`,
+        });
+      }
+
+      break;
+
+    case "products":
+      model = await Product.findById(id);
+      if (!model) {
+        return res.status(400).json({
+          msg: `No existe un producto con el id ${id}`,
+        });
+      }
+
+      break;
+
+    default:
+      return res.status(500).json({ msg: "No he validado esto" });
+  }
+
+  // Clean preview images
+  if ( model.img ){
+    // // Delete server image
+    // const pathImage = path.join( __dirname, '../uploads', collection, model.img );
+    // if ( fs.existsSync(pathImage) ){
+    //   fs.unlinkSync(pathImage);
+    // }
+  }
+
+  const { tempFilePath } = req.files.archivo;
+  const { secure_url } = await cloudinary.uploader.upload(tempFilePath);
+
+  model.img = secure_url;
+
+  await model.save();
+
+  res.json(model);
+};
 module.exports = {
   uploadFile,
   updateImage,
-  viewImage
+  viewImage,
+  updateImageCloudinary
 };
--- routes/uploads.router.js
+++ routes/uploads.router.js
@@ -2,7 +2,7 @@ const { Router } = require("express");
 const {check} = require("express-validator");
 
 const { validateFields, validateUploadFile} = require('../middlewares')
-const { uploadFile, updateImage, viewImage} = require("../controllers/uploads.contoller");
+const { uploadFile, updateImage, viewImage, updateImageCloudinary} = require("../controllers/uploads.contoller");
 const {permittedCollection} = require("../helpers");
 
 
@@ -15,7 +15,8 @@ router.put('/:collection/:id',[
     check('id', 'El id debe ser de mongo').isMongoId(),
     check('collection').custom(c => permittedCollection(c ,['users', 'products'])),
     validateFields
-], updateImage);
+], updateImageCloudinary);
+// ], updateImage);
 
 router.get('/:collection/:id', [
     check('id', 'El id debe ser de mongo').isMongoId(),
